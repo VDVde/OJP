6dbdeb2900853cea5c73a48b026c0632104628bd
Günter Matthias (IT-SWE-CC3-TS2 - Extern) <matthias.guenter@sbb.ch>
2020-10-10 18:04:57 +0200

    travis adaptions and a 3rd example

--- .travis/xmllint-check.sh
+++ .travis/xmllint-check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 /usr/bin/find . -type f \( -name "*.xsd" -or -name "*.xml" \) -print0 | while read -r -d $'\0' filename; do XMLLINT_INDENT=$'\t' xmllint --encode UTF-8 --pretty 1 "${filename}" > "${filename}.pretty"; mv "${filename}.pretty" "${filename}"; grep -i -v "xmlspy" "${filename}" > "${filename}.tmp" && mv "${filename}.tmp" "${filename}"; done;
 echo "finished formatting"
-# xmllint --noout --schema OJP.xsd examples/subdirectory1/*xml examples/subdirectory2/*xml
-xmllint --noout --schema OJP.xsd examples/test_examples_switzerland/*xml 
+# for each test set a different subdirectory can be created.
+xmllint --noout --schema OJP.xsd examples/*/*.xml
+
 
