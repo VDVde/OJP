602fd2f73823e1c750602d43ad166d80e87dd387
Stephan Großberndt <s.grossberndt@sidebysite.de>
2020-12-03 16:29:11 +0100

    Added automatic generation of documentation tables using Travis

--- docs/generate-tables.sh
+++ docs/generate-tables.sh
@@ -4,6 +4,11 @@
 # You need the binary `xsltproc` to convert from xsd to asciidoc and `asciidoctor` to convert from asciidoc to html
 # apt-get install xsltproc asciidoctor
 
+# The -e flag causes the script to exit as soon as one command returns a non-zero exit code
+set -e
+
+echo "Generating documentation tables"
+
 base_dir="$(dirname "${0}")/.."
 xsd_dir=$base_dir
 xsl_dir=$base_dir/docs
@@ -12,6 +17,15 @@ generated_dir="${base_dir}/docs/generated"
 # prepare generated_dir
 mkdir -p "${generated_dir}"
 rm -f "${generated_dir}"/*.adoc "${generated_dir}"/*.html
+
+# Add asciidoc header
+printf '%s\n' \
+  '= OJP - **O**pen API for distributed **J**ourney **P**lanning' \
+ ":toc:" \
+ ":homepage: https://github.com/VDVde/OJP" \
+ "// Disable last updated text." \
+ ":!last-update-label:" \
+ "" >> "${generated_dir}"/OJP.adoc
 
 # create asciidoc file
 xsltproc "${xsl_dir}"/ojp-to-asciidoc.xsl \
@@ -31,7 +45,7 @@ xsltproc "${xsl_dir}"/ojp-to-asciidoc.xsl \
  "${xsd_dir}"/OJP/OJP_FacilitySupport.xsd \
  "${xsd_dir}"/OJP/OJP_ModesSupport.xsd \
  "${xsd_dir}"/OJP/OJP_Utility.xsd \
- > "${generated_dir}"/OJP.adoc
+ >> "${generated_dir}"/OJP.adoc
 
 # convert asciidoc to html5
 asciidoctor "${generated_dir}/OJP.adoc"
