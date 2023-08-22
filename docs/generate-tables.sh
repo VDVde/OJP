cc55ea93257bcdc105f957404131f4fead1f60b2
Stefan de Konink <stefan@konink.de>
2023-08-22 09:45:43 +0200

    Firs XCore test.

--- docs/generate-tables.sh
+++ docs/generate-tables.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 # Generate the documentation tables as docs/generated/OJP.html from the .xsd schema files
 #
-# You need the binary `xsltproc`
+# You need the binary `java`
 # apt-get install xsltproc
 
 # The -e flag causes the script to exit as soon as one command returns a non-zero exit code
 set -e
 
-echo "Generating documentation tables ..."
-
 base_dir="$(dirname "${0}")/.."
 xsl_dir=$base_dir/docs
 generated_dir="${base_dir}/docs/generated"
+basex="/tmp/basex.jar"
+
+echo "Download BaseX ..."
+
+if [ ! -e ${basex} ]; then
+	wget --output-document=${basex} https://files.basex.org/releases/10.6/BaseX106.jar
+fi
+
+echo "Generating documentation tables ..."
 
 # prepare generated_dir
 mkdir -p "${generated_dir}"
-rm -f "${generated_dir}"/OJP-prep.xml "${generated_dir}"/*.adoc "${generated_dir}"/*.html
-cp "${xsl_dir}"/asciidoc.css "${generated_dir}"/
-
-# create intermediate XML file for documentation
-xsltproc --xinclude "${xsl_dir}"/ojp-to-prepdoc.xsl \
- "${xsl_dir}"/schema-collection.xml \
- >> "${generated_dir}"/OJP-prep.xml
-
-# generate stand-alone HTML file for documentation
-xsltproc --xinclude "${xsl_dir}"/ojp-prep-to-html-with-toc.xsl \
- "${generated_dir}"/OJP-prep.xml \
- >> "${generated_dir}"/index.html
+rm -f "${generated_dir}"/contab/*.html
+java -cp /tmp/basex.jar org.basex.BaseX -b report=contab -b dir=${base_dir} -b odir=${generated_dir} -b custom=${xsl_dir}/custom-ojp.xml -b dnamesExcluded=".git .github" xcore.xq
+rm -fr "${generated_dir}"/edesc
 
 echo -e '\033[0;32mFinished generating documentation tables\033[0m'
